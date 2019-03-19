2ddcf2d6d2c4d53591185d3551154f823930ee3e
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2019-03-19 12:55:13 -0600

    Inicio del proyecto

---
+++ src/main/java/com/tech/pro/backend/apirest/BackendApiretApplication.java
@@ -0,0 +1,13 @@
+package com.tech.pro.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApiretApplication.class, args);
+	}
+
+}
