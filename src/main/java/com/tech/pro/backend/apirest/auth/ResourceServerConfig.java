ec36414aebfe22b8e15ed1d82bb97ed469291987
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2019-03-27 09:24:52 -0600

    Cuarto paso de auth

---
+++ src/main/java/com/tech/pro/backend/apirest/auth/ResourceServerConfig.java
@@ -0,0 +1,18 @@
+package com.tech.pro.backend.apirest.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(http);
+	}
+	
+}
