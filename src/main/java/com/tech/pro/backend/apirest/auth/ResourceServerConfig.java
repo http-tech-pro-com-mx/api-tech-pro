87f9451adfb9dd399fddb3b4afe653dcfc4bbf53
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2019-03-27 13:08:01 -0600

    Generacion del token

--- src/main/java/com/tech/pro/backend/apirest/auth/ResourceServerConfig.java
+++ src/main/java/com/tech/pro/backend/apirest/auth/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.tech.pro.backend.apirest.auth;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -11,8 +12,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/usuarios/findAll").permitAll()
+		.anyRequest().authenticated();
 	}
 	
 }
