89242567f138997f9f796090e3579a04fac831ab
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2019-04-04 18:08:10 -0600

    Envio de parametros json

--- src/main/java/com/tech/pro/backend/apirest/controller/UsuarioRestController.java
+++ src/main/java/com/tech/pro/backend/apirest/controller/UsuarioRestController.java
@@ -6,13 +6,19 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.node.TextNode;
 import com.tech.pro.backend.apirest.models.entity.Area;
 import com.tech.pro.backend.apirest.models.entity.Usuario;
 import com.tech.pro.backend.apirest.services.AreaServiceImpl;
@@ -54,6 +60,16 @@ public class UsuarioRestController {
 		
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 		
-	} 
+	}
+	
+	@Secured({"ROLE_CAMBIAR_PWD"})
+	@PostMapping(path="/changePassword")
+	public ResponseEntity<?> changePassword(@RequestBody  Map<String,String> params){
+		Map<String, Object> response =  new HashMap<>();
+		response.put("actual", params.get("actual"));
+		response.put("nueva", params.get("nueva"));
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+		
+	}
 	
 }
