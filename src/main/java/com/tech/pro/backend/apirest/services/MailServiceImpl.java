d636513296a1007e6bef5afde6a03c63425aa56a
Ramiro Luna <rlunaaragon@gmail.com>
2019-07-04 17:24:47 -0500

    Cambio de envio de correo electronico

--- src/main/java/com/tech/pro/backend/apirest/services/MailServiceImpl.java
+++ src/main/java/com/tech/pro/backend/apirest/services/MailServiceImpl.java
@@ -6,13 +6,18 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Env;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MailServiceImpl {
-
+	
+	@Value("${spring.mail.username}")
+	private String from;
+	
 	@Autowired
 	private JavaMailSender javaMailSender;
 
@@ -21,6 +26,7 @@ public class MailServiceImpl {
 		MimeMessage message = javaMailSender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
 		message.setContent(texto, "text/html; charset=utf-8");
+		helper.setFrom(from);
 		helper.setTo(correo);
 		helper.setSubject(subject);
 
@@ -33,6 +39,7 @@ public class MailServiceImpl {
 		MimeMessage message = javaMailSender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
 		message.setContent(texto, "text/html; charset=utf-8");
+		helper.setFrom(from);
 		helper.setTo(correos);
 		helper.setSubject(subject);
 
